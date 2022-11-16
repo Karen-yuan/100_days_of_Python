7bdbd39b8c403c694c65467b9f1549152753f7dc
KarenYuan1227 <yuan.jiax@northeastern.edu>
2022-11-16 17:06:33 -0500

    first commit

---
+++ Day7/multiple_ex.py
@@ -0,0 +1,22 @@
+word_list= ["ardvardk","baboon","camel"]
+import random
+chosen_word= random.choice(word_list)
+
+print(f"Pssst, the solution is {chosen_word}.")
+display=[]
+word_lengh=len(chosen_word)
+for _ in range(word_lengh):
+    display=display+"_"
+print(display)
+end_of_game= False
+while not end_of_game:
+guess=input("guess a letter:").lower()
+for position in range(word_lengh):
+    letter=chosen_word[position]
+    print(f"Current position: {position}\n Current letter:{letter}\n Guessed letter: {guess}")
+    if letter== guess:
+        display[position]=letter
+print(display)
+if "_" not in display:
+    end_of_game= True
+    print("You win.")
