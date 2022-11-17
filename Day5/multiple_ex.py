8f07b0219f2055d7e529a34dc8291aced09144a9
KarenYuan1227 <yuan.jiax@northeastern.edu>
2022-11-17 17:01:01 -0500

    first commit

--- Day5/multiple_ex.py
+++ Day5/multiple_ex.py
@@ -1,52 +1,51 @@
-# student_scores=input("input a list of student scores").split()
-# for n in range(0,len(student_scores)):
-#     student_scores[n]= int(student_scores[n])
-# maxi_score=student_scores[0]
-# for i in student_scores:
-#     if i> maxi_score:
-#         maxi_score=i
-# print("the highest score in the class is:", maxi_score)
-# total=0
-# for number in range(1,101):
-#     total+= number
-# print(total)
-# total=0
-# for number in range(2,101,2):
-#     total+= number
-# print(total)
-
-# for number in range(1,101):
-#     if number%15==0:
-#         print("FizzBuzz")
-#     elif number%3==0:
-#         print("Fizz")
-#     elif number%5==0:
-#         print("Buzz")
-#     else:
-#         print(number)
+# student highest score
 import random
-letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-numbers=['0','1','2','3','4','5','6','7','8','9']
-symbols=['!','#','$','%','&','(',')','*','+']
+student_scores = input("input a list of student scores").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+maxi_score = student_scores[0]
+for i in student_scores:
+    if i > maxi_score:
+        maxi_score = i
+print("the highest score in the class is:", maxi_score)
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+total = 0
+for number in range(2, 101, 2):
+    total += number
+print(total)
+
+# FizzBuzz game
+for number in range(1, 101):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+# random password
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 print("welcome to the pypassword generator!")
-nr_letters=int(input("how many letters would you like in your password?\n"))
-nr_numbers=int(input("how many symbols would you like?\n"))
-nr_symbols=int(input("how many numbers would you like?\n"))
-password=""
-for char in range(1,nr_letters+1):
-    random_char= random.choice(letters)
-    password=password +random_char
+nr_letters = int(input("how many letters would you like in your password?\n"))
+nr_numbers = int(input("how many symbols would you like?\n"))
+nr_symbols = int(input("how many numbers would you like?\n"))
+password = ""
+for char in range(1, nr_letters+1):
+    random_char = random.choice(letters)
+    password = password + random_char
 
-for char in range(1,nr_numbers+1):
-    random_char= random.choice(numbers)
-    password=password +random_char
-    
-for char in range(1,nr_symbols+1):
-    random_char= random.choice(symbols)
-    password=password +random_char
+for char in range(1, nr_numbers+1):
+    random_char = random.choice(numbers)
+    password = password + random_char
+
+for char in range(1, nr_symbols+1):
+    random_char = random.choice(symbols)
+    password = password + random_char
 print(password)
-
-    
-
-
-    
--- Day7/multiple_ex.py
+++ Day7/multiple_ex.py
@@ -1,22 +1,25 @@
-word_list= ["ardvardk","baboon","camel"]
+# random word exercise:
 import random
-chosen_word= random.choice(word_list)
-
+word_list = ["ardvardk", "baboon", "camel"]
+chosen_word = random.choice(word_list)
 print(f"Pssst, the solution is {chosen_word}.")
-display=[]
-word_lengh=len(chosen_word)
+
+# guess letter game
+display = []
+word_lengh = len(chosen_word)
 for _ in range(word_lengh):
-    display=display+"_"
+    display = display+"_"
 print(display)
-end_of_game= False
+end_of_game = False
 while not end_of_game:
-guess=input("guess a letter:").lower()
+    guess = input("guess a letter:").lower()
 for position in range(word_lengh):
-    letter=chosen_word[position]
-    print(f"Current position: {position}\n Current letter:{letter}\n Guessed letter: {guess}")
-    if letter== guess:
-        display[position]=letter
+    letter = chosen_word[position]
+    print(
+        f"Current position: {position}\n Current letter:{letter}\n Guessed letter: {guess}")
+    if letter == guess:
+        display[position] = letter
 print(display)
 if "_" not in display:
-    end_of_game= True
+    end_of_game = True
     print("You win.")
